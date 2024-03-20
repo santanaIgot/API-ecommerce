584895e3075b1d6f53b60dc816198196b8993b26
santanaIgot <santanaigoor01@gmail.com>
2024-03-19 22:33:47 -0300

    Feat - Created class client with variables and get and setters

---
+++ Aula04/src/main/java/br/com/fiap/store/Aula04/model/Cliente.java
@@ -0,0 +1,42 @@
+package br.com.fiap.store.Aula04.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+//entitiy indica que ela é uma tabela no banco
+@Getter@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "aula04_tb_cliente")
+@EntityListeners(AuditingEntityListener.class) // framework vai criar valor de forma automatica
+public class Cliente {
+    @Id
+    @GeneratedValue
+    @Column(name="cd_codigo")
+    private Long codigo;
+    @Transient //indica que algo n vai ser uma coluna no bd
+    private  String token;
+    @Column(name="nm_cliente", nullable = false, length = 100, unique = true)
+    private String nome;
+    @Column(name="nr_cpf", unique = true, length = 11, nullable = false)
+    private String cpf;
+    @Column(name="nascimento", nullable = false)
+    private LocalDate nascimento;
+    @Column(name="categoria_cliente", nullable = false, length = 30)
+    @Enumerated(EnumType.STRING)
+    private CategoriaCliente categoriaCliente;
+    @Column(name="pontos", precision = 10)
+    private Integer pontos;
+    @CreatedDate
+    @Column(name = "dt_cadastro")
+    private LocalDateTime dataCadastro;
+
+
+}
