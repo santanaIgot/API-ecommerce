507b3196cdc34014aeed788f010ac59947537ddb
santanaIgot <santanaigoor01@gmail.com>
2024-03-19 22:33:47 -0300

    Feat - Creating method post

---
+++ Aula04/src/main/java/br/com/fiap/store/Aula04/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.store.Aula04.repository;
+
+import br.com.fiap.store.Aula04.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository <Cliente,Integer>    {
+
+}
